2cb512039496a5cfc4f1ba49c8a22e064de9ec84
Harvey Mei <harvey.mei@msn.com>
2021-05-07 12:24:06 +0800

    Commit Message

--- mpl_squares.py
+++ mpl_squares.py
@@ -13,9 +13,16 @@ Matplotlib is a comprehensive library for creating static, animated, and interac
 
 # https://matplotlib.org/stable/api/pyplot_summary.html
 # https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html#matplotlib.pyplot.plot
+# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.show.html#matplotlib.pyplot.show
 # 导入模块并设置别名
 import matplotlib.pyplot as plt
 
+# 赋值列表
 squares = [1, 4, 9, 16, 25]
+
+# Plot y versus x as lines and/or markers.
+# 调用plt.plot方法并传入参数
 plt.plot(squares)
+# Display all open figures.
+# 显示图形
 plt.show()
