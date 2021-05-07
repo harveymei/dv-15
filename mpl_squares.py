c4481c2c6f16dc34f6b8e453883298025cdd2e88
Harvey Mei <harvey.mei@msn.com>
2021-05-07 11:48:10 +0800

    Initial commit

---
+++ mpl_squares.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time    : 2021/5/7 11:34 上午
+# @Author  : Harvey Mei <harvey.mei@msn.com>
+# @FileName: mpl_squares.py
+# @IDE     : PyCharm
+# @GitHub  : https://github.com/harveymei/
+
