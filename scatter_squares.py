00cff816c3cba2a396de24708775b3152f7c662c
Harvey Mei <harvey.mei@msn.com>
2021-05-07 15:21:44 +0800

    Commit Message

---
+++ scatter_squares.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Time    : 2021/5/7 3:10 下午
+# @Author  : Harvey Mei <harvey.mei@msn.com>
+# @FileName: scatter_squares.py
+# @IDE     : PyCharm
+# @GitHub  : https://github.com/harveymei/
+
+"""
+绘制散点图
+"""
+import matplotlib.pyplot as plt
+
+# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.scatter.html#matplotlib.pyplot.scatter
+# x, y: float or array-like, shape (n, )
+# s: float or array-like, shape (n, ), optional
+plt.scatter(2, 4)
+plt.show()
