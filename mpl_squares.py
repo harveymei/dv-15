1a9b154c008bdabf8b9c9c111b4d691dc7764731
Harvey Mei <harvey.mei@msn.com>
2021-05-07 12:14:56 +0800

    Commit Message

--- mpl_squares.py
+++ mpl_squares.py
@@ -6,3 +6,16 @@
 # @IDE     : PyCharm
 # @GitHub  : https://github.com/harveymei/
 
+"""
+Visualization with Python¶
+Matplotlib is a comprehensive library for creating static, animated, and interactive visualizations in Python.
+"""
+
+# https://matplotlib.org/stable/api/pyplot_summary.html
+# https://matplotlib.org/stable/api/_as_gen/matplotlib.pyplot.plot.html#matplotlib.pyplot.plot
+# 导入模块并设置别名
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+plt.plot(squares)
+plt.show()
