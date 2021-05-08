1705722ffe5ba4977a716b1051019267988a4b7e
Harvey Mei <harvey.mei@msn.com>
2021-05-08 15:47:53 +0800

    Commit Message

--- rw_visual3.py
+++ rw_visual3.py
@@ -15,14 +15,19 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:  # 循环
-    rw = RandomWalk()
+    # rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
+
+    # 设置绘图窗口尺寸，单位英寸
+    # plt.figure(figsize=(10, 6))
+    plt.figure(dpi=128, figsize=(10, 6))
 
     # plt.scatter(rw.x_values, rw.y_values, s=5)
     # 给点着色，根据点的生成顺序由浅蓝色到深蓝色
     point_numbers = list(range(rw.num_points))  # 生成一个0-4999的列表
     # 向颜色参数c传递列表值，指定颜色映射为蓝色，去除点的黑色轮廓
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=5)
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
 
     # 突出起点和终点，在已绘制完的点上重新绘制起点和终点
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
